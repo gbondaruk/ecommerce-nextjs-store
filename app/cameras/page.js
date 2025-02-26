069b0f0542cf546f3bc46fc91fc493fccc380022
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-02-26 21:34:04 +0100

    Styling and database changes

--- app/cameras/page.js
+++ app/cameras/page.js
@@ -12,28 +12,33 @@ export default async function CamerasPage() {
   const cameras = await getCamerasInsecure();
 
   return (
-    <div className={styles.cameraPage}>
-      <h1>Cameras</h1>
-
-      {cameras.map((camera) => {
-        return (
-          <div key={`camera-${camera.id}`}>
-            <Link href={`/cameras/${camera.id}`}>
-              <div>
-                {camera.brand} {camera.title}
-              </div>
-              <Image
-                src={`/images/${camera.title.toLowerCase()}.png`}
-                alt={camera.title}
-                width={300}
-                height={200}
-              />
-              <div>Price: €{camera.price}</div>
-              <br />
-            </Link>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className={styles.cameraPageH1}>
+        <h1>Vintage Cameras</h1>
+      </div>
+      <div className={styles.cameraPage}>
+        {cameras.map((camera) => {
+          return (
+            <div className={styles.singleCameraDiv} key={`camera-${camera.id}`}>
+              <Link href={`/cameras/${camera.id}`}>
+                <div>
+                  <h2>
+                    {camera.brand} {camera.title}
+                  </h2>
+                </div>
+                <Image
+                  src={`/images/${camera.title.toLowerCase()}.png`}
+                  alt={camera.title}
+                  width={300}
+                  height={200}
+                />
+                <div>Price: €{camera.price}</div>
+                <br />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
